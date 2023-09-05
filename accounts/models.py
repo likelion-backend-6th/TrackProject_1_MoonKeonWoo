df354f259c38a74aabe4bb39360e3cb958549d95
Keonwoo Moon <pureunkkum53@gmail.com>
2023-09-06 01:15:49 +0900

    not null 제약조건 위반 오류 수정

--- accounts/models.py
+++ accounts/models.py
@@ -3,11 +3,7 @@ from django.db import models
 
 # Create your models here.
 class CustomUser(AbstractUser):
-    email = models.EmailField(
-        verbose_name='email',
-        max_length=255,
-        unique=True,
-    )
-    date_of_birth = models.DateField(blank=True)
+    email = models.EmailField(verbose_name='email', max_length=255, unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
 
 
