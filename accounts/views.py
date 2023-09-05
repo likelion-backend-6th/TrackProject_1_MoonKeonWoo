04650e827ed3e62e82d4aacd19d49c348f62927f
Keonwoo Moon <pureunkkum53@gmail.com>
2023-09-06 01:21:32 +0900

    SignUpView와 회원가입 폼 템플릿 생성

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+
+from accounts.forms import CustomUserCreationForm
+
 
 # Create your views here.
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    # success_url = reverse_lazy("login")
+    template_name = "accounts/signup.html"
